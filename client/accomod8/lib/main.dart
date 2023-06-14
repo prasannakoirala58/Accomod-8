e3f5c839cce10d02fe5f0e24aad5658034da708b
ishan sapkota <ishansapkota46@gmail.com>
2023-06-14 10:28:48 +0545

    logo rendered, login and sign up pages success

--- client/accomod8/lib/main.dart
+++ client/accomod8/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:accomod8/pages/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,61 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Accomod8',
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Accomod8'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: LogInScreen(),
     );
   }
 }
---
+++ client/accomod8/lib/pages/signup_screen.dart
@@ -0,0 +1,174 @@
+import 'package:accomod8/pages/login_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+//import 'package:flutter_svg/flutter_svg.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  bool passToggle = true;
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Image.asset(
+                  "images/acclog.png",
+                  height: 100,
+                  width: 150,
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 6, horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Full Name",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person)),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 6, horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Email Address",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email)),
+                ),
+              ),
+              const SizedBox(height: 5),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 6, horizontal: 15),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Phone Number",
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.phone)),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  //yaha we obscure password wala text so that no one can see it
+                  obscureText: passToggle ? true : false,
+                  decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      label: const Text("Enter Password"),
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          if (passToggle == true) {
+                            passToggle == false;
+                          } else {
+                            passToggle == true;
+                          }
+                          setState(() {});
+                        },
+                        child: passToggle
+                            ? const Icon(CupertinoIcons.eye_slash_fill)
+                            : const Icon(CupertinoIcons.eye_fill),
+                      )),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  //yaha we obscure password wala text so that no one can see it
+                  obscureText: passToggle ? true : false,
+                  decoration: InputDecoration(
+                      border: const OutlineInputBorder(),
+                      label: const Text("Confirm Password"),
+                      prefixIcon: const Icon(Icons.lock),
+                      suffixIcon: InkWell(
+                        onTap: () {
+                          if (passToggle == true) {
+                            passToggle == false;
+                          } else {
+                            passToggle == true;
+                          }
+                          setState(() {});
+                        },
+                        child: passToggle
+                            ? const Icon(CupertinoIcons.eye_slash_fill)
+                            : const Icon(CupertinoIcons.eye_fill),
+                      )),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Material(
+                color: Color.fromARGB(255, 242, 162, 131),
+                borderRadius: BorderRadius.circular(10),
+                child: InkWell(
+                  onTap: () {
+                    //Navigator.push(
+                    //context,
+                    //MaterialPageRoute(
+                    //builder: (context) => SignUpScreen(),
+                    //)); //Navigator.push(context, MaterialPageRoute(builder: (context)));
+                  },
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 40),
+                    child: Text(
+                      "Create Account",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Already Have an Account?",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black54),
+                  ),
+                  //here create an account wala button press garyo vane we will go to signup screen
+                  TextButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const LogInScreen(),
+                          ));
+                    },
+                    child: const Text("Goto Login",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 242, 162, 131),
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
